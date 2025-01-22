f1d6cee2615e767f0ffd661efbc2d75fe07db8c4
WuLiuSeven <125783608+WuLiuSeven@users.noreply.github.com>
2025-01-22 01:45:51 -0500

    Floyd's js code, Untested

--- js/code.js
+++ js/code.js
@@ -115,6 +115,125 @@ function doLogin()
 
 }
 
+function doAdd(){
+	//let newContact = document.getElementById("createContact").value; //
+	let firstName = document.getElementById("firstName").value;
+	let lastName = document.getElementById("lastName").value;
+	let phone = document.getElementById("phone").value;
+	let email = document.getElementById("email").value;
+	let userId = document.getElementById("userId").value;
+	document.getElementById("createContactResult").innerHTML = ""; //
+
+
+	let tmp = {
+		FirstName: firstName,
+		LastName: lastName,
+		Phone: phone,
+		Email: email,
+		UserID: userId
+	};
+
+	//let tmp = {color:newContact,userId,userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/Add.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("createContactResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("createContactResult").innerHTML = err.message;
+	}
+}
+
+function doDelete(){
+	let databaseId = document.getElementById("deleteContact").value; //
+	document.getElementById("deleteContactResult").innerHTML = ""; //
+
+	let tmp = {id: databaseId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	let url = urlBase + '/Delete.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("deleteContactResult").innerHTML = "Contact has been deleted";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("deleteContactResult").innerHTML = err.message;
+	}
+}
+function doEdit(){
+
+}
+
+function doSearch(){
+	let srch = document.getElementById("searchBar").value;
+	document.getElementById("searchButton").innerHTML = ""; //
+	
+	let list = "";
+
+	let tmp = {search:srch,UserID:userId};
+	let jsonPayload = JSON.stringify( tmp );
+
+	//url to the php file (urlbase/Search.php)
+	let url = urlBase + '/Search.' + extension;
+	
+	let xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("searchButton").innerHTML = "Contact(s) has been retrieved"; //
+				let jsonObject = JSON.parse( xhr.responseText );
+				
+				for( let i=0; i<jsonObject.results.length; i++ )
+				{
+					list += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						list += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = list;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("SearchResult").innerHTML = err.message;
+	}
+}
+
 function saveCookie()
 {
 	let minutes = 20;
