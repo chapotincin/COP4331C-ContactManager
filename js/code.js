0e04fbf4e70be02ab802880df783edc3bba876ed
mayacou <mayalynnx@hotmail.com>
2025-02-03 20:06:30 -0500

    Save button fix

--- js/code.js
+++ js/code.js
@@ -241,6 +241,7 @@ function doEdit(button){
 
 	//now we need a save button to confirm the changes
 	let saveButton = document.createElement('button');
+	saveButton.classList.add('save-btn');
 	saveButton.textContent = 'Save'; //replace with save icon
 	saveButton.setAttribute('onclick', `doSave(${eID})`); //onclick for saving
 	editRow.cells[4].innerHTML = ''; //replace the edit button with save button
